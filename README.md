1888522ec926ae1e6aa9acccb11c402f8d2291ec
richardhainsworth <richard01@outlook.com>
2020-07-17 23:45:52 +0100

    Enhanced the input validation further

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
  
 Project Title: Guessing Game
 Make run at: 
-Fri 17 Jul 2020 21:49:56 BST
+Fri 17 Jul 2020 23:44:31 BST
 guessinggame.sh contains the following lines of code:
-37
+41
--- guessinggame.sh
+++ guessinggame.sh
@@ -5,22 +5,26 @@ correct_answer=$(ls -la | awk '{print $9}' | grep -E "[^\.]|[^\.\.]" | wc -l | g
  echo "How many files (including directories, symlinks and hidden files) are there in the current directory?" 
  echo "(the current directory is: $(pwd))"
 
+
 function guessing_game {
  read response
+ response_sanitised=$(echo $response | grep -Eo "\d+$")
+ response_test=$(expr $response + 0) 
 }
 
 
 guessing_game
 
-  while [[ $response -ne $correct_answer ]]
+  while [[ $response_sanitised -ne $correct_answer ]]
 	do
-		if [[ $response =~ [^0-9]+ ]]
-			then	
+		
+		if [[ ! $response_test =~ ^[0-9]+$ ]]  
+			then
 				echo "Please enter a number in the decimal (base 10) system"
 				guessing_game
 		fi
 
-		if [[ $response -gt $correct_answer ]]
+		if [[ $response_sanitised -gt $correct_answer ]]
 			then
 				echo "You entered $response, which is too high!  Please try again!"
 				guessing_game
@@ -30,7 +34,7 @@ guessing_game
 		fi		
   done
 
-  if [[ $response -eq $correct_answer ]]
+  if [[ $response_sanitised -eq $correct_answer ]]
 	then
 		echo "You entered $response, the total files in the directory is also $correct_answer"
 		echo "Well done - you guessed correctly!"
